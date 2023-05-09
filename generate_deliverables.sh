5a0c4b52283e94ed03451c83e1c57594a8c93f49
Clara Daia <claradaiahd@gmail.com>
2023-05-09 15:04:50 -0230

    add fuel prices script to deliverables

--- generate_deliverables.sh
+++ generate_deliverables.sh
@@ -2,6 +2,7 @@
 DATE=`date +%d%b%Y`
 
 rm deliverables/*
-cp ClaraMAEssaydraft.do deliverables/ClaraMAEssaydraft$DATE.do 
+cp ClaraMAEssaydraft.do deliverables/ClaraMAEssaydraft$DATE.do
+cp fuel_prices_plot.do deliverables/fuel_prices_plot$DATE.do
 cp aaaMUN/SUPERVISION/2022/Clara/ClaraMAEssaybibfile.bib deliverables/ClaraMAEssaybibfile$DATE.bib
 cp aaaMUN/SUPERVISION/2022/Clara/ClaraMAEssaydraft.pdf deliverables/ClaraMAEssaydraft$DATE.pdf
