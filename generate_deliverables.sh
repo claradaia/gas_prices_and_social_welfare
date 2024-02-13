496afb304c7a5dbd1474a85274d23b1e39f1d708
Clara Daia <claradaiahd@gmail.com>
2024-02-12 19:47:00 -0500

    fix gen deliverables
    * do not add date to bibfile name (could add a placeholder but too much
      work)
    * stash and pop to prevent unwanted changes from being included

--- generate_deliverables.sh
+++ generate_deliverables.sh
@@ -2,9 +2,11 @@
 DATE=`date +%d%b%Y`
 
 rm deliverables/*
+git stash
 cp ClaraMAEssaydraft.do deliverables/ClaraMAEssaydraft$DATE.do
 cp fuel_prices_plot.do deliverables/fuel_prices_plot$DATE.do
-cp aaaMUN/SUPERVISION/2022/Clara/ClaraMAEssaybibfile.bib deliverables/ClaraMAEssaybibfile$DATE.bib
+cp aaaMUN/SUPERVISION/2022/Clara/ClaraMAEssaybibfile.bib deliverables/ClaraMAEssaybibfile.bib
 cp aaaMUN/SUPERVISION/2022/Clara/ClaraMAEssaydraft.pdf deliverables/ClaraMAEssaydraft$DATE.pdf
 cp aaaMUN/SUPERVISION/2022/Clara/ClaraMAEssaydraft.tex deliverables/ClaraMAEssaydraft$DATE.tex
+git stash pop
 
