541f6b2dfe0e06b7e56aea0b6c40ee14a75e6eed
Clara Daia <claradaiahd@gmail.com>
2024-04-26 23:12:09 -0400

    add scheme file to deliverables

--- generate_deliverables.sh
+++ generate_deliverables.sh
@@ -7,4 +7,5 @@ cp fuel_prices_plot.do deliverables/fuel_prices_plot$DATE.do
 cp aaaMUN/SUPERVISION/2022/Clara/ClaraMAEssaybibfile.bib deliverables/ClaraMAEssaybibfile.bib
 cp aaaMUN/SUPERVISION/2022/Clara/ClaraMAEssaydraft.pdf deliverables/ClaraMAEssaydraft$DATE.pdf
 cp aaaMUN/SUPERVISION/2022/Clara/ClaraMAEssaydraft.tex deliverables/ClaraMAEssaydraft$DATE.tex
+cp aaaMUN/SUPERVISION/2022/Clara/scheme-clara.scheme deliverables/scheme-clara.scheme
 
